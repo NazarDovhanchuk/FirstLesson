486658eaf84dab676f810277ab0b3624b4b92e74
Nazar <nazardovhanchuk@gmail.com>
2020-09-14 22:20:14 +0300

    first version

---
+++ script.js
@@ -0,0 +1,71 @@
+// Task #2.
+
+// let surname = 'Dovhanchuk';
+
+// alert(surname);
+
+// Task #3.
+
+// let firstVariable = 'First';
+// let secondVariable = 'Second';
+
+// alert(firstVariable);
+// alert(secondVariable);
+
+// firstVariable = secondVariable;
+
+// alert(firstVariable);
+
+
+// Task #4
+
+// let myObj = {
+//   str: 'String',
+//   num: 'Number',
+//   trueOfFalse: 'Boolean',
+//   fourType: undefined,
+//   fiveType: null
+// }
+
+
+// Task #5.
+
+// let isAdult = confirm('Вам виповнилось 18років?')
+
+// console.log(isAdult)
+
+
+// Task #6.
+
+// let name = 'Nazar';  // string 
+// let surName = 'Dovhanchuk'; // Чи також вивести в консоль через typeOf? Щоб показати, що я розумію який це тип
+// let group = 'Lv-533.JS Core';
+// let yearOfbirth = 1994;
+// let maritalStatus = false;
+
+// let fourType = null;
+// let fiveType;
+
+// console.log(typeof yearOfbirth);
+// console.log(typeof maritalStatus);
+// console.log(typeof group);
+// console.log(typeof fourType);  // object --> null
+// console.log(typeof fiveType);
+
+
+// Task #7.
+
+// let login = prompt("Введіть Ваш логін");
+// let email = prompt("Введіть Вашу електронну скриньку");
+// let password = prompt("Введіть Ваш пароль");
+
+// let result = alert(`Dear ${login} your email is ${email} your password is ${password}`)
+
+
+// Task #8.
+
+let secondOfHour = 60 * 60;
+let secondOfDay = secondOfHour * 24;
+let secondOfMonth = secondOfDay * 30;
+
+let result = alert(`Second of hour ${secondOfHour}, day ${secondOfDay}, month ${secondOfMonth}`)
